8dc4717dfac09f68854dcebd513847d2969162d5
Milk3-25 <59041258+Milk3-25@users.noreply.github.com>
2020-10-22 19:26:09 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator_v._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+//            Your program will ask the user for 2 integers and then display a menu giving a choice of:
+//A - Addition
+//S - Subraction
+//M - Multiplication
+//D - Division
+//The program will then ask the user which operation is wanted(Ask for A, S, M or D to be typed in.)
+//                    The output will show the 2 numbers that were entered, and the result of the operation.
+//If any other character or decimal number is input, display an appropriate error message and end the program.
+//For full marks, your code must also be fully annotated.
+//Remember: **From now on all assignments will require full program development => PD(Program Development) Table or Flowchart prior to beginning Coding**
+//When your program is completed show Mr.Van De Wiele your code to see it in operation
+//You will be submitting your program code by copying it into a word document and submitting it to D2L and your flowchart or a PD table can be done by hand or on the computer.  Submit to Mr.Van De Wiele by  D2L or by hand as appropriate.
+//Any questions ask in class.
+        }
+    }
+}
