395dabc508bc2e92ff6e5b0d548b2cb26abb8294
Milk3-25 <59041258+Milk3-25@users.noreply.github.com>
2020-10-22 19:28:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Hello to your verry own interest calculator!\nplease enter your interest rate (%)");
+                // turns the user number entered into a decimal
+                decimal interestRatepercent = decimal.Parse
+                    (Console.ReadLine());
+                // converts to a interest rate percentage 
+                decimal interestRate = interestRatepercent / 100;
+                Console.WriteLine("Please enter your amount owed ($)\nTRUTHFULLY");
+                // turns the user number entered into a decimal
+                decimal amountOwed = decimal.Parse(Console.ReadLine());
+                // input user values for them to view
+                Console.WriteLine("Interest rate = " + interestRatepercent + "%");
+                Console.WriteLine("Amount owed = " + amountOwed + "$");
+                // calculates amount of interest
+                decimal interest = interestRate * amountOwed;
+                // creates a generic text to be used repeatedly
+                string total = "Your amount owed is ";
+                // my if statements
+                if (interest > 10) Console.WriteLine(total + interest + "$");
+                else if (amountOwed < 10) Console.WriteLine(total + amountOwed + "$");
+                else if (10 > interest || 10 > amountOwed) Console.WriteLine(total + "10$");
+
+                Console.ReadLine();
+        }
+    }
+}
